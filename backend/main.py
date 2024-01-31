bd3e481ee64273b18472e5b26880f81873e6bf05
NeverlandMusic <denniskollmann98@gmail.com>
2024-01-31 14:38:33 +0100

    feat: update structure

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,11 @@ from ImageClassifier import ImageClassifier
 from fastapi import FastAPI
 
 
-URL = "http://192.168.0.50/image.bmp"
-
 app = FastAPI()
 
+classifier = ImageClassifier()
 
 @app.get("/prediction-stats/class")
 async def get_class():
-    classifier = ImageClassifier()
     class_name = classifier.predict_class()
     return {"class_name": class_name}
